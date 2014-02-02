871ceae28fdff996dd7d93b1c6d2bb561e4f8686
Alberto Campos <xopmac@gmail.com>
2014-02-02 08:54:36 -0800

    initial commit
    Based on Tim’s Twitter Client

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 3
+
+// AFOAuth1Client
+#define COCOAPODS_POD_AVAILABLE_AFOAuth1Client
+#define COCOAPODS_VERSION_MAJOR_AFOAuth1Client 0
+#define COCOAPODS_VERSION_MINOR_AFOAuth1Client 2
+#define COCOAPODS_VERSION_PATCH_AFOAuth1Client 1
+
+// NZAlertView
+#define COCOAPODS_POD_AVAILABLE_NZAlertView
+#define COCOAPODS_VERSION_MAJOR_NZAlertView 0
+#define COCOAPODS_VERSION_MINOR_NZAlertView 0
+#define COCOAPODS_VERSION_PATCH_NZAlertView 5
+
+// UIImage-Helpers
+#define COCOAPODS_POD_AVAILABLE_UIImage_Helpers
+#define COCOAPODS_VERSION_MAJOR_UIImage_Helpers 0
+#define COCOAPODS_VERSION_MINOR_UIImage_Helpers 0
+#define COCOAPODS_VERSION_PATCH_UIImage_Helpers 1
+
